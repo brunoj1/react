08ea1641f2af6c91e44c86e0d4386d6f734d2a73
byamasaki <byamasaki@AMLDKTPR45KWSC>
2018-12-07 18:04:34 -0200

    Alterações Curso

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -1,26 +1,67 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, Button } from 'react';
 import UserOutput from './UserOutput'
 import UserInput from './UserInput'
 
 class Assignment extends Component {
-    state = { userName: "Bruno" };
+    state = 
+    { 
+        showUsers: false,
+        messageShow: "Show",
+        persons: [
+                    {
+                        name: "Bruno",
+                        description: "Yamasaki"
+                    }, {
+                        name: "Carla",
+                        description: "Etc"
+                    }
+                ]
+        };
+
+    deletePersonhandler = (personIndex) => {
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
+    }
 
     inputHandler = (event) => {
         const value = event.target.value;
         console.log(value);
-        this.setState({ userName: value});
+        let persons = [...this.state.persons];
+        if (persons.length > 0)
+            persons[0].name = value;
+
+        this.setState({ persons: persons});
+    }
+
+    togglePersonHandler = () => {
+        const doesShow = this.state.showUsers;        
+
+        this.setState(
+            {
+                showUsers: !doesShow, 
+                messageShow: doesShow  ? "Show": "Hide"
+            });
     }
 
     render() {
+        let users = null;
+
+        if (this.state.showUsers) {
+            users = (<div>
+             {this.state.persons.map((person, key) => 
+                <UserOutput click={this.deletePersonhandler.bind(this)} key={key} userName={person.name}>{person.description}</UserOutput>
+             )}</div>)};
+
         return ( 
             <React.Fragment>
+                <button className="btn btn-primary" onClick={this.togglePersonHandler.bind(this)}>{this.state.messageShow}</button>
+                <br/><br/>
+
                 <UserInput change={this.inputHandler.bind(this)}/>
-                <UserOutput userName="Ana">Um</UserOutput>
-                <UserOutput userName={this.state.userName}>Dois</UserOutput>
-                <UserOutput userName="Carla">Três</UserOutput>
+                {users}
             </React.Fragment>
         )}
 }
 
 export default Assignment;
-// teste2
--- src/components/UserOutput.js
+++ src/components/UserOutput.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+const divStyle = {
+    margin: '10px'
+};
+
 const UserOutput = props => 
     (
-        <div className="teste">
-            <p>{props.userName}</p>
-            <p>{props.children}</p>
+        <div className="card" style={divStyle}>        
+            <div className="card-body">
+                <h5 className="card-title">{props.userName}</h5>
+                <p className="card-text">{props.children}</p>
+                <button onClick={props.click} className="btn btn-danger">Remove</button>
+            </div>
         </div>
     );
 
